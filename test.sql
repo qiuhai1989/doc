3b0a61b19bf1f54827535e6ce17335beb2b14a41
qiuhai <55704123ggdd@163.com>
2016-04-02 11:17:14 +0800

    提交test.sql

---
+++ test.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE `animal` (
+`id`  int NOT NULL AUTO_INCREMENT ,
+`name`  varchar(255) NULL ,
+`type`  varchar(255) NULL ,
+`value`  varchar(20) NULL ,
+`create_time`  timestamp NULL ,
+`modify_time`  timestamp  NULL,
+PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8
+;
+
+insert into animal (name,type,value,create_time,modify_time)values('tiger','cat','0',now(),NOW());
